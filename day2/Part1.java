81990d6bb69b073907fb9531d4027eaa177b4898
Justin Gianelloni <justin.gianelloni@gmail.com>
2025-06-15 19:29:19 -0400

    Day 2

--- day2/Part1.java
+++ day2/Part1.java
@@ -1,4 +1,68 @@
 package aoc.day2;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class Part1 {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        String file = "day2.txt";
+        Path path = Path.of(ClassLoader.getSystemResource(file).toURI());
+        String[] lines = readInput(path);
+        Integer answer = parseInput(lines);
+        System.out.println("The answer is: " + answer);
+    }
+
+    private static String[] readInput(Path path) throws IOException {
+        List<String> lines = Files.readAllLines(path);
+        return lines.toArray(new String[0]);
+    }
+
+    private static Integer parseInput(String[] lines) {
+        int answer = 0;
+        for (String line : lines) {
+            String[] chars = line.split(" ");
+            Integer[] numbers = new Integer[chars.length];
+            for (int i = 0; i < chars.length; i++) {
+                numbers[i] = Integer.parseInt(chars[i]);
+            }
+            if (isArraySafe(numbers)) {
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+
+    private static Boolean isArraySafe(Integer[] numbers) {
+        return isArrayMonotonic(numbers) && isArrayClose(numbers);
+    }
+
+    private static Boolean isArrayMonotonic(Integer[] numbers) {
+        boolean ascending = true;
+        boolean descending = true;
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > numbers[i-1]) {
+                descending = false;
+            }
+            if (numbers[i] < numbers[i-1]) {
+                ascending = false;
+            }
+        }
+        return ascending || descending;
+    }
+
+    private static Boolean isArrayClose(Integer[] numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+            if (Math.abs(numbers[i] - numbers[i-1]) < 1) {
+                return false;
+            }
+            if (Math.abs(numbers[i] - numbers[i-1]) > 3) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
